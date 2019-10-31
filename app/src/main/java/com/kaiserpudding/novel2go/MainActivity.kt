f254f8ccbea5465cdd63905ae7eec876c2617421
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-10-31 20:05:49 +0100

    Add listener to Start button and start download

--- app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.kaiserpudding.novel2go
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.kaiserpudding.novel2go.downloader.CruxDownloader
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val view =  super.onCreateView(name, context, attrs)
+
+        val button = view?.findViewById<Button>(R.id.start)
+        button?.setOnClickListener {
+            val url = view.findViewById<EditText>(R.id.edit_text_url).text.toString()
+            val email = view.findViewById<EditText>(R.id.edit_text_email).text.toString()
+
+            val downloader = CruxDownloader()
+            downloader.download(url)
+        }
+        return view
+    }
+
 }
