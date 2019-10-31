d5ed2105059bfd617739a36a80806b9bf7756c1a
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-10-31 20:25:09 +0100

    Create PdfCreator class and convert to companion functions

--- app/src/main/java/com/kaiserpudding/novel2go/downloader/PdfCreator.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/downloader/PdfCreator.kt
@@ -7,37 +7,40 @@ import rst.pdfbox.layout.elements.Document
 import rst.pdfbox.layout.elements.Paragraph
 import java.io.FileOutputStream
 
+class PdfCreator {
+    companion object {
+        /**
+         * Creates a PDF document from the given jsoup document.
+         *
+         * @param jsoupDocument
+         */
+        fun createPdf(jsoupDocument: JsoupDocument) {
+            val document = Document(40f, 50f, 40f, 60f)
+            val paragraphs = parseParagraphs(jsoupDocument.childNodes())
+            for (text in paragraphs) {
+                val paragraph = Paragraph()
+                paragraph.addText(text, 12f, PDType1Font.TIMES_ROMAN)
+                document.add(paragraph)
+            }
+            val outputStream = FileOutputStream("generated/file.pdf")
+            document.save(outputStream)
+        }
 
-/**
- * Creates a PDF document from the given jsoup document.
- *
- * @param jsoupDocument
- */
-fun createPdf(jsoupDocument: JsoupDocument) {
-    val document = Document(40f, 50f, 40f, 60f)
-    val paragraphs = parseParagraphs(jsoupDocument.childNodes())
-    for (text in paragraphs) {
-        val paragraph = Paragraph()
-        paragraph.addText(text, 12f, PDType1Font.TIMES_ROMAN)
-        document.add(paragraph)
+        const val htmlTagRegex = "<p>|</p>"
+
+        /**
+         * Converts the list of nodes to a list of strings with the content of the nodes
+         * @see htmlTagRegex
+         * @param nodes
+         * @return Return List of content of the Nodes
+         */
+        fun parseParagraphs(nodes: List<Node>): List<String> {
+            val paragraphs = mutableListOf<String>()
+            val regex = Regex(htmlTagRegex)
+            for (text in nodes) {
+                paragraphs.add(text.toString().replace(regex, ""))
+            }
+            return paragraphs
+        }
     }
-    val outputStream = FileOutputStream("generated/file.pdf")
-    document.save(outputStream)
-}
-
-const val htmlTagRegex = "<p>|</p>"
-
-/**
- * Converts the list of nodes to a list of strings with the content of the nodes
- * @see htmlTagRegex
- * @param nodes
- * @return Return List of content of the Nodes
- */
-fun parseParagraphs(nodes: List<Node>): List<String> {
-    val paragraphs = mutableListOf<String>()
-    val regex = Regex(htmlTagRegex)
-    for (text in nodes) {
-        paragraphs.add(text.toString().replace(regex, ""))
-    }
-    return paragraphs
 }
