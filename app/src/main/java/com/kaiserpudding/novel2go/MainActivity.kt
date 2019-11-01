190eee3f9a7c4025c4e7e425ccbf0ed6b5d8af4e
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-01 11:12:49 +0100

    WIP Replace pdf libraries with native android library

--- app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
@@ -3,31 +3,42 @@ package com.kaiserpudding.novel2go
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.kaiserpudding.novel2go.downloader.CruxDownloader
+import com.kaiserpudding.novel2go.downloader.PdfCreator
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+//import com.kaiserpudding.novel2go.downloader.PdfCreator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        val view =  super.onCreateView(name, context, attrs)
+        val button = findViewById<Button>(R.id.button_start)
+        button.setOnClickListener {
 
-        val button = view?.findViewById<Button>(R.id.start)
-        button?.setOnClickListener {
-            val url = view.findViewById<EditText>(R.id.edit_text_url).text.toString()
-            val email = view.findViewById<EditText>(R.id.edit_text_email).text.toString()
 
-            val downloader = CruxDownloader()
-            downloader.download(url)
+            val context = this
+            val job = GlobalScope.launch {
+                val url = findViewById<EditText>(R.id.edit_text_url).text.toString()
+                val email = findViewById<EditText>(R.id.edit_text_email).text.toString()
+                val downloader = CruxDownloader()
+                val article = downloader.download(url)
+                val file = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                file?.mkdirs()
+
+                if (file != null) {
+                    PdfCreator.createPdf(article.document, file)
+                }
+            }
         }
-        return view
     }
-
 }
--- app/src/main/java/com/kaiserpudding/novel2go/downloader/PdfCreator.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/downloader/PdfCreator.kt
@@ -1,29 +1,50 @@
 package com.kaiserpudding.novel2go.downloader
 
-import org.apache.pdfbox.pdmodel.font.PDType1Font
+//import de.wirecard.pdfbox.layout.elements.Document
+//import de.wirecard.pdfbox.layout.elements.Paragraph
+//import org.apache.pdfbox.pdmodel.font.PDType1Font
+//import org.jsoup.nodes.Node
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
+import android.os.Environment
+import android.util.Log
 import org.jsoup.nodes.Node
-import org.jsoup.nodes.Document as JsoupDocument
-import rst.pdfbox.layout.elements.Document
-import rst.pdfbox.layout.elements.Paragraph
+import java.io.File
 import java.io.FileOutputStream
+import org.jsoup.nodes.Document as JsoupDocument
 
 class PdfCreator {
+
     companion object {
-        /**
-         * Creates a PDF document from the given jsoup document.
-         *
-         * @param jsoupDocument
-         */
-        fun createPdf(jsoupDocument: JsoupDocument) {
-            val document = Document(40f, 50f, 40f, 60f)
+        fun createPdf(jsoupDocument: JsoupDocument, filesDir: File) {
+            val document = PdfDocument()
+            val pageInfo = PdfDocument.PageInfo.Builder( 210, 2970, 1).create()
+            val page = document.startPage(pageInfo)
+            val canvas = page.canvas
+            val paint = Paint()
+            paint.color = Color.BLACK
+            paint.textSize = 5f
+            paint.isLinearText = true
+
             val paragraphs = parseParagraphs(jsoupDocument.childNodes())
-            for (text in paragraphs) {
-                val paragraph = Paragraph()
-                paragraph.addText(text, 12f, PDType1Font.TIMES_ROMAN)
-                document.add(paragraph)
+
+            var multiplier = 1
+            for (paragraph : String in paragraphs) {
+                canvas.drawText(paragraph, 20f, 20f + 5f * multiplier, paint)
+                multiplier++
             }
-            val outputStream = FileOutputStream("generated/file.pdf")
-            document.save(outputStream)
+            document.finishPage(page)
+
+//            val path = filesDir
+//            val file = File(path)
+//            if (!file.exists()) {
+//                file.mkdirs()
+//            }
+//            val filePath = File("$path/myfile.pdf")
+            Log.d("write_file", "written to $filesDir")
+            document.writeTo(FileOutputStream("$filesDir/myfile.pdf"))
+            document.close()
         }
 
         const val htmlTagRegex = "<p>|</p>"
@@ -43,4 +64,24 @@ class PdfCreator {
             return paragraphs
         }
     }
+//    companion object {
+//        /**
+//         * Creates a PDF document from the given jsoup document.
+//         *
+//         * @param jsoupDocument
+//         */
+//        fun createPdf(jsoupDocument: JsoupDocument) {
+//            val document = Document(40f, 50f, 40f, 60f)
+//            val paragraphs = parseParagraphs(jsoupDocument.childNodes())
+//            for (text in paragraphs) {
+//                val paragraph = Paragraph()
+//                paragraph.addText(text, 12f, PDType1Font.TIMES_ROMAN)
+//                document.add(paragraph)
+//            }
+//            val outputStream = FileOutputStream("generated/file.pdf")
+//            document.save(outputStream)
+//        }
+
+
+//    }
 }
