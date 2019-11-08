442af7f3cc7d1081d23e88d43c708c89567acd9c
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-08 16:46:53 +0100

    Change filename to title of extracted article

--- app/src/main/java/com/kaiserpudding/novel2go/extractor/Extractor.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/extractor/Extractor.kt
@@ -8,9 +8,8 @@ class Extractor {
         val downloader = CruxDownloader()
         val article = downloader.download(url)
         if (!file.exists()) file.mkdirs()
-        val fileName = "filenName.pdf"
         val pdfCreator = PdfCreator()
-        pdfCreator.createPdf(article.document, file, fileName)
-        return fileName
+        pdfCreator.createPdf(article.document, file, article.title)
+        return article.title
     }
 }
