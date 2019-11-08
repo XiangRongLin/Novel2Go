99357c01858c82fe53ae50a751da9ff3a7222b30
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-08 20:07:17 +0100

    Clear url editText after starting

--- app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
@@ -3,10 +3,14 @@ package com.kaiserpudding.novel2go
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.core.graphics.contains
 import com.kaiserpudding.novel2go.extractor.Extractor
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -20,9 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button_start)
         button.setOnClickListener {
+            val urlEditText = findViewById<EditText>(R.id.edit_text_url)
+            val url = urlEditText.text.toString()
+            urlEditText.setText("")
+            val email = findViewById<EditText>(R.id.edit_text_email).text.toString()
             GlobalScope.launch {
-                val url = findViewById<EditText>(R.id.edit_text_url).text.toString()
-                val email = findViewById<EditText>(R.id.edit_text_email).text.toString()
                 val file = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
                 val extractor = Extractor()
                 val fileName = extractor.extractSingle(url, file!!)
