8c9f11e384fb3effefdd610d59055d669ed6919a
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-09 18:51:17 +0100

    Use jetpack navigation with fragments

---
+++ app/src/main/java/com/kaiserpudding/novel2go/DownloadFragment.kt
@@ -0,0 +1,98 @@
+package com.kaiserpudding.novel2go
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Environment
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.FileProvider
+import com.kaiserpudding.novel2go.extractor.Extractor
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.File
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [DownloadFragment.OnDownloadInteractionListener] interface
+ * to handle interaction events.
+ * Use the [DownloadFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DownloadFragment : Fragment() {
+    private var listener: OnDownloadInteractionListener? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.button_start)
+        button.setOnClickListener {
+            val urlEditText = view.findViewById<EditText>(R.id.edit_text_url)
+            val url = urlEditText.text.toString()
+            urlEditText.setText("")
+            val email = view.findViewById<EditText>(R.id.edit_text_email).text.toString()
+            GlobalScope.launch {
+                val file = activity?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+                val extractor = Extractor()
+                val fileName = extractor.extractSingle(url, file!!)
+                startEmailIntent(email, file, fileName)
+            }
+        }
+    }
+
+    private fun startEmailIntent(email: String, file: File, fileName: String) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "convert")
+        val uri = FileProvider.getUriForFile(
+            context!!,
+            BuildConfig.APPLICATION_ID + ".fileprovider",
+            File(file, fileName)
+        )
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        intent.type = "message/rfc/822"
+        startActivity(Intent.createChooser(intent, "send mail"))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_download, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnDownloadInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnDownloadInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnDownloadInteractionListener {
+
+    }
+}
--- app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
@@ -1,53 +1,24 @@
 package com.kaiserpudding.novel2go
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.MotionEvent
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import androidx.core.graphics.contains
-import com.kaiserpudding.novel2go.extractor.Extractor
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.File
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    DownloadFragment.OnDownloadInteractionListener{
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button_start)
-        button.setOnClickListener {
-            val urlEditText = findViewById<EditText>(R.id.edit_text_url)
-            val url = urlEditText.text.toString()
-            urlEditText.setText("")
-            val email = findViewById<EditText>(R.id.edit_text_email).text.toString()
-            GlobalScope.launch {
-                val file = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
-                val extractor = Extractor()
-                val fileName = extractor.extractSingle(url, file!!)
-                startEmailIntent(email, file, fileName)
-            }
-        }
+        val host =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = host.navController
+
     }
 
-    private fun startEmailIntent(email: String, file: File, fileName: String) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-        intent.putExtra(Intent.EXTRA_SUBJECT, "convert")
-        val uri = FileProvider.getUriForFile(
-            this,
-            BuildConfig.APPLICATION_ID + ".fileprovider",
-            File(file, fileName)
-        )
-        intent.putExtra(Intent.EXTRA_STREAM, uri)
-        intent.type = "message/rfc/822"
-        startActivity(Intent.createChooser(intent, "send mail"))
-    }
+
 }
