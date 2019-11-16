119007fc526606b37532bf9ff00a0010d505ea6a
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-16 10:47:04 +0100

    Show current saved email address as summary

--- app/src/main/java/com/kaiserpudding/novel2go/SettingsFragment.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/SettingsFragment.kt
@@ -1,13 +1,10 @@
 package com.kaiserpudding.novel2go
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 /**
  * A simple [Fragment] subclass.
@@ -19,5 +16,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference, rootKey)
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val kindleEmailPreference = findPreference<EditTextPreference>("kindle_email")
+        kindleEmailPreference?.summary = preferences.getString("kindle_email", "")
     }
 }
