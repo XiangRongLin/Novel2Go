d98671d77a5027a4ca2064bd467805ada6777a1d
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-16 09:52:07 +0100

    Add basic navigation drawer
    With option for "Home" and "Settings"
    Settings doesn't navigate anywhere yet

--- app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/MainActivity.kt
@@ -1,24 +1,46 @@
 package com.kaiserpudding.novel2go
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(),
-    DownloadFragment.OnDownloadInteractionListener{
+    DownloadFragment.OnDownloadInteractionListener {
 
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.drawer_layout)
         val host =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = host.navController
 
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener{item ->
+            item.isChecked = true
+            drawerLayout.closeDrawers()
+
+            when (item.itemId) {
+                R.id.home -> navController.navigate(R.id.fragment_download)
+//            R.id.settings ->
+            }
+            true
+        }
+
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
 }
