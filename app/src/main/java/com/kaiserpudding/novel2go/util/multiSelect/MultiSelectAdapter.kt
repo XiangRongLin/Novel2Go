e6c3558ddaea29b5b1490c8d37f4c7ff9373672c
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-17 15:16:21 +0100

    Add MultiSelect Fragment&Adapter and SafeClickListener
    Copied from HowTheyWrite (previous project)

---
+++ app/src/main/java/com/kaiserpudding/novel2go/util/multiSelect/MultiSelectAdapter.kt
@@ -0,0 +1,120 @@
+package com.kaiserpudding.novel2go.util.multiSelect
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kaiserpudding.novel2go.util.multiSelect.MultiSelectAdapter.MultiSelectAdapterItemInteractionListener
+import com.kaiserpudding.novel2go.util.setSafeOnClickListener
+
+/**
+ * An adapter handling the logic needed for a recycler view with clickable and selectable items.
+ *
+ * @param T They type of the items shown in the list
+ * @property listener A listener implementing [MultiSelectAdapterItemInteractionListener]
+ * handling onClick and onLongClick actions on the items
+ */
+abstract class MultiSelectAdapter<T>(
+    private val listener: MultiSelectAdapterItemInteractionListener
+) : RecyclerView.Adapter<MultiSelectAdapter<T>.MultiSelectViewHolder>() {
+
+    private val selectedIdSet = mutableSetOf<Long>()
+    val selectedIdArray: LongArray
+        get() = selectedIdSet.toLongArray()
+    val numberOfSelected: Int
+        get() = selectedIdArray.size
+    val inSelectionMode: Boolean
+        get() = numberOfSelected > 0
+
+    /**
+     * The id of the layout that is representing a single item in the recycler view
+     */
+    protected abstract val viewHolderId: Int
+
+    /**
+     * The list of the items that are represented in the recycler view
+     */
+    var list: List<T>? = null
+
+
+    override fun onBindViewHolder(holder: MultiSelectViewHolder, position: Int) {
+        //if item at position is selected, activate it and thus show different background color
+        holder.itemView.isActivated = selectedIdSet.contains(getMyItemId(position))
+    }
+
+    /**
+     * Remove/Add the id of the item at [position] from [selectedIdSet] depending on if its already in there.
+     * Then notifies listeners of changes.
+     *
+     * @param position
+     */
+    fun toggleSelectedThenNotify(position: Int) {
+        val id = getMyItemId(position)
+        if (selectedIdSet.contains(id)) selectedIdSet.remove(id)
+        else selectedIdSet.add(id)
+        listener.onDataSetChanged()
+        notifyItemChanged(position)
+    }
+
+    /**
+     * Clear [selectedIdSet] and then notifies listeners of changes.
+     *
+     */
+    fun clearSelectedThenNotify() {
+        selectedIdSet.clear()
+        listener.onDataSetChanged()
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Return the id of the item at [position]
+     *
+     * @param position
+     * @return The id of the item
+     */
+    protected abstract fun getMyItemId(position: Int): Long
+
+    /**
+     * Method to create a [MultiSelectViewHolder].
+     * Used to avoid the inner class restriction.
+     *
+     * @param view
+     * @return
+     */
+    protected fun createViewHolder(view: View): MultiSelectViewHolder {
+        return MultiSelectViewHolder(view)
+    }
+
+    inner class MultiSelectViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val textView: TextView = view.findViewById(viewHolderId)
+
+        init {
+            view.setSafeOnClickListener {
+                if (inSelectionMode) toggleSelectedThenNotify(adapterPosition)
+                else listener.onMultiSelectAdapterInteraction(getMyItemId(adapterPosition))
+            }
+            view.setOnLongClickListener {
+                toggleSelectedThenNotify(adapterPosition); true
+            }
+        }
+    }
+
+    /**
+     * Interface which must be implemented to get notified of data changes and adapter interaction
+     *
+     */
+    interface MultiSelectAdapterItemInteractionListener {
+
+        /**
+         * Gets called when an item was selected or deselected.
+         */
+        fun onDataSetChanged()
+
+        /**
+         * Gets called when an item was clicked on while adapter is not [inSelectionMode]
+         *
+         * @param itemId The id of the item that was clicked.
+         */
+        fun onMultiSelectAdapterInteraction(itemId: Long)
+    }
+}
---
+++ app/src/main/java/com/kaiserpudding/novel2go/util/multiSelect/MultiSelectFragment.kt
@@ -0,0 +1,89 @@
+package com.kaiserpudding.novel2go.util.multiSelect
+
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+
+/**
+ * A Fragment handling the logic needed for a recycler view with clickable and selectable items.
+ *
+ * Implements the [MultiSelectAdapter.MultiSelectAdapterItemInteractionListener] and basic things
+ * of [ActionMode] to display a bar at the top when something is selected.
+ *
+ * @param T The type of the items shown in the list.
+ * @param A The type of the adapter. Must be subclass of [MultiSelectAdapter]
+ */
+abstract class MultiSelectFragment<T, A : MultiSelectAdapter<T>> : Fragment(),
+    MultiSelectAdapter.MultiSelectAdapterItemInteractionListener {
+
+    protected lateinit var adapter: A
+    protected var actionMode: ActionMode? = null
+
+    /**
+     * The id of the menu shown in the [ActionMode].
+     */
+    protected abstract val actionMenuId: Int
+
+    /**
+     * [ActionMode.Callback] implementation which relies on subclasses implementing
+     * [actionMenuId] and [onMyActionItemClicked].
+     */
+    private val actionModeCallback: ActionMode.Callback =
+        object : ActionMode.Callback {
+            override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+                val inflater = mode.menuInflater
+                inflater.inflate(actionMenuId, menu)
+                mode.title = "${adapter.numberOfSelected} selected"
+                return true
+            }
+
+            override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+                return false
+            }
+
+            override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+                return onMyActionItemClicked(mode, item)
+            }
+
+            override fun onDestroyActionMode(mode: ActionMode) {
+                adapter.clearSelectedThenNotify()
+                actionMode = null
+            }
+        }
+
+    /**
+     * Implementation for the [ActionMode.Callback.onActionItemClicked] method.
+     * Needs to handle click on items of menu with [actionMenuId].
+     *
+     * @param mode The current ActionMode
+     * @param item The item that was clicked
+     * @return true if this callback handled the event, false if the standard MenuItem invocation should continue.
+     */
+    protected abstract fun onMyActionItemClicked(mode: ActionMode, item: MenuItem): Boolean
+
+    override fun onStop() {
+        super.onStop()
+        actionMode?.finish()
+        adapter.clearSelectedThenNotify()
+    }
+
+    protected abstract fun onListInteraction(itemId: Long)
+
+    /**
+     * Start/Stop/Continue action mode depending on if [MultiSelectAdapter.inSelectionMode]
+     * and update [ActionMode.setTitle] if action mode is still active.
+     */
+    override fun onDataSetChanged() {
+        if (adapter.inSelectionMode) {
+            if (actionMode == null) actionMode = activity?.startActionMode(actionModeCallback)
+            actionMode?.title = "${adapter.numberOfSelected} selected"
+        } else {
+            actionMode?.finish()
+        }
+    }
+
+    override fun onMultiSelectAdapterInteraction(itemId: Long) {
+        onListInteraction(itemId)
+    }
+}
