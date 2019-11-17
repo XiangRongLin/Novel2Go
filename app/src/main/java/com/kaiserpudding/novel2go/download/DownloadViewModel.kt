4d57ad1287cb74b9d02f1f22033bcb71466939f9
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-17 09:52:04 +0100

    Add basic download view model

---
+++ app/src/main/java/com/kaiserpudding/novel2go/download/DownloadViewModel.kt
@@ -0,0 +1,25 @@
+package com.kaiserpudding.novel2go.download
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.kaiserpudding.novel2go.database.AppDatabase
+import com.kaiserpudding.novel2go.model.Download
+import com.kaiserpudding.novel2go.repositories.DownloadRepository
+import kotlinx.coroutines.launch
+
+class DownloadViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: DownloadRepository =
+        DownloadRepository(AppDatabase.getDatabase(application).downloadDao())
+    val allDownloads: LiveData<List<Download>>
+
+    init {
+        allDownloads = repository.getAll()
+    }
+
+    fun insert(download: Download) = viewModelScope.launch {
+        repository.insert(download)
+    }
+}
