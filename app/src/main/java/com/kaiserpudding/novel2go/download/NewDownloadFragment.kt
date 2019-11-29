ec741776cb08eedde23692786c06e7e842c69d2a
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-11-29 11:42:29 +0100

    Remove immediatly sharing after finishing downloads and stay in fragment
    Leaving fragment causes entry to not get insertet into db

--- app/src/main/java/com/kaiserpudding/novel2go/download/NewDownloadFragment.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/download/NewDownloadFragment.kt
@@ -1,7 +1,6 @@
 package com.kaiserpudding.novel2go.download
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
@@ -9,11 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.preference.PreferenceManager
-import com.kaiserpudding.novel2go.BuildConfig
 import com.kaiserpudding.novel2go.R
 import com.kaiserpudding.novel2go.extractor.Extractor
 import com.kaiserpudding.novel2go.model.Download
@@ -37,16 +33,16 @@ class NewDownloadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val button = view.findViewById<Button>(R.id.button_start)
         button.setOnClickListener {
             val urlEditText = view.findViewById<EditText>(R.id.edit_text_url)
             val url = urlEditText.text.toString()
-//            listener!!.onStartDownload()
             GlobalScope.launch {
                 val file = activity?.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
                 val extractor = Extractor()
                 val fileName = extractor.extractSingle(url, file!!)
-                val tmp =  File(file, "$fileName.pdf")
+                val tmp = File(file, "$fileName.pdf")
                 downloadViewModel.insert(
                     Download(
                         fileName,
@@ -56,29 +52,10 @@ class NewDownloadFragment : Fragment() {
                         System.currentTimeMillis()
                     )
                 )
-                startEmailIntent(file, fileName)
+
             }
+//            listener!!.onStartDownload()
         }
-    }
-
-    private fun startEmailIntent(file: File, fileName: String) {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        val email = prefs.getString("kindle_email", "")
-        if (!email.isNullOrEmpty()) {
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-            val useConvert = prefs.getBoolean("use_amazon_convert", true)
-            if (useConvert) intent.putExtra(Intent.EXTRA_SUBJECT, "convert")
-            val uri = FileProvider.getUriForFile(
-                context!!,
-                BuildConfig.APPLICATION_ID + ".fileprovider",
-                File(file, "$fileName.pdf")
-            )
-            intent.putExtra(Intent.EXTRA_STREAM, uri)
-            intent.type = "message/rfc/822"
-            startActivity(Intent.createChooser(intent, "send mail"))
-        }
-
     }
 
     override fun onCreateView(
