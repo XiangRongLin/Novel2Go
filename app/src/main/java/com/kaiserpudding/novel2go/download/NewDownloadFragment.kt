70121748834b7a8a5ce9280bcf845fe462e4dbc6
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-12-01 09:16:39 +0100

    hide keyboard after starting download

--- app/src/main/java/com/kaiserpudding/novel2go/download/NewDownloadFragment.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/download/NewDownloadFragment.kt
@@ -4,17 +4,19 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.IBinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.kaiserpudding.novel2go.BuildConfig.EXTERNAL_STORAGE_PERMISSION
 import com.kaiserpudding.novel2go.R
 import com.kaiserpudding.novel2go.download.service.DownloadService
+import kotlinx.android.synthetic.main.fragment_new_download.*
 
 /**
  * A simple [Fragment] subclass.
@@ -38,8 +40,7 @@ class NewDownloadFragment : Fragment() {
 
         val button = view.findViewById<Button>(R.id.button_start)
         button.setOnClickListener {
-            val urlEditText = view.findViewById<EditText>(R.id.edit_text_url)
-            val url = urlEditText.text.toString()
+            val url = edit_text_url.text.toString()
             val storagePermission = ContextCompat.checkSelfPermission(
                 context!!, android.Manifest.permission.WRITE_EXTERNAL_STORAGE
             ) != PackageManager.PERMISSION_DENIED
@@ -52,6 +53,7 @@ class NewDownloadFragment : Fragment() {
                 )
                 activity?.startService(it)
             }
+            hideKeyboardAndDefocus(view.windowToken)
             listener!!.onStartDownload()
         }
     }
@@ -62,6 +64,17 @@ class NewDownloadFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_download, container, false)
+    }
+
+    /**
+     * Clear focus of quizInputEditText and hide keyboard
+     *
+     * @param view
+     */
+    private fun hideKeyboardAndDefocus(windowToken: IBinder) {
+        edit_text_url.clearFocus()
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 
     override fun onAttach(context: Context) {
