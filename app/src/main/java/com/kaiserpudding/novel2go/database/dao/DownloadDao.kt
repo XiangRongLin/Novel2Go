21aabbc90ed4d7759dc8461d71e09f452e91c667
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2019-12-06 16:31:49 +0100

    order downloads from new to old

--- app/src/main/java/com/kaiserpudding/novel2go/database/dao/DownloadDao.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/database/dao/DownloadDao.kt
@@ -7,7 +7,7 @@ import com.kaiserpudding.novel2go.model.Download
 @Dao
 interface DownloadDao : BaseDao<Download> {
 
-    @Query("SELECT * FROM downloads" )
+    @Query("SELECT * FROM downloads ORDER BY timestamp DESC" )
     fun getAll() : LiveData<List<Download>>
 
     @Query("DELETE FROM downloads WHERE id IN (:downloads)")
