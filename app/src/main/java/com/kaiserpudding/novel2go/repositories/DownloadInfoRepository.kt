2acca2a8571acf5bc3b14438f99fe0fbcb22df8f
Xiang Rong Lin <41164160+XiangRongLin@users.noreply.github.com>
2020-05-11 12:23:14 +0200

    Use NonCancelable for inserts and deletes
    Solve cases of viewmodel being destroyed before those operations finish

--- app/src/main/java/com/kaiserpudding/novel2go/repositories/DownloadInfoRepository.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/repositories/DownloadInfoRepository.kt
@@ -3,18 +3,19 @@ package com.kaiserpudding.novel2go.repositories
 import com.kaiserpudding.novel2go.database.dao.DownloadInfoDao
 import com.kaiserpudding.novel2go.model.DownloadInfo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.withContext
 
 class DownloadInfoRepository(private val downloadInfoDao: DownloadInfoDao) {
 
     suspend fun insert(downloadInfo: DownloadInfo): Long {
-        return withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO + NonCancellable) {
             return@withContext downloadInfoDao.insert(downloadInfo)
         }
     }
 
     suspend fun insert(downloadInfos: Collection<DownloadInfo>) {
-        return withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO + NonCancellable) {
             return@withContext downloadInfoDao.insert(downloadInfos)
         }
     }
--- app/src/main/java/com/kaiserpudding/novel2go/repositories/DownloadRepository.kt
+++ app/src/main/java/com/kaiserpudding/novel2go/repositories/DownloadRepository.kt
@@ -4,24 +4,25 @@ import androidx.lifecycle.LiveData
 import com.kaiserpudding.novel2go.database.dao.DownloadDao
 import com.kaiserpudding.novel2go.model.Download
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.withContext
 
 class DownloadRepository(private val downloadDao: DownloadDao) {
 
     suspend fun insert(download: Download): Long {
-        return withContext(Dispatchers.IO) {
+        return withContext(Dispatchers.IO + NonCancellable) {
             return@withContext downloadDao.insert(download)
         }
     }
 
     suspend fun delete(download: Download) {
-        withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO + NonCancellable) {
             downloadDao.delete(download)
         }
     }
 
     suspend fun delete(downloads: LongArray) {
-        withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO + NonCancellable) {
             downloadDao.delete(downloads)
         }
     }
